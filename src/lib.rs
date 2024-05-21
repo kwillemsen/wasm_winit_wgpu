647682a463f8640549d571bf6c7c878658c9945e
Koen Willemsen <Koen.Willemsen@materialise.be>
2024-05-21 13:12:40 +0200

    Add `chrono` and clarify code structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
+fn system_now() -> String {
+    chrono::Local::now().to_rfc3339()
+}
+
 #[cfg(target_family = "wasm")]
 pub mod wasm {
+    use super::*;
     use wasm_bindgen::prelude::*;
 
     #[wasm_bindgen(start)]
-    pub fn main() -> Result<(), JsValue> {
+    pub fn wasm_main() -> Result<(), JsValue> {
         std::panic::set_hook(Box::new(console_error_panic_hook::hook));
         console_log::init_with_level(log::Level::Debug)
             .expect("console_log::init_with_level() failed");
-        log::info!("entering main()...");
+        log::info!("entering wasm_main() at {}...", system_now());
         let window = web_sys::window().expect("web_sys::window() failed");
         log::info!("let window = web_sys::window() succeeded");
         let document = window.document().expect("window.document() failed");
@@ -23,27 +28,28 @@ pub mod wasm {
             .get_element_by_id("rust_canvas")
             .expect("let canvas = document.get_element_by_id(\"rust_canvas\") failed");
         log::info!("let canvas = document.get_element_by_id(\"rust_canvas\") succeeded");
-        let canvas: web_sys::HtmlCanvasElement = canvas
+        let _canvas: web_sys::HtmlCanvasElement = canvas
             .dyn_into::<web_sys::HtmlCanvasElement>()
             .expect("let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>() failed");
         log::info!("let canvas: web_sys::HtmlCanvasElement = canvas.dyn_into::<web_sys::HtmlCanvasElement>() succeeded");
 
-        log::info!("...exiting main()");
+        log::info!("...exiting wasm_main() at {}", system_now());
         Ok(())
     }
 }
 
 #[cfg(not(target_family = "wasm"))]
 pub mod not_wasm {
-    pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use super::*;
+    pub fn desktop_main() -> Result<(), Box<dyn std::error::Error>> {
         env_logger::init();
-        log::info!("entering main()...");
-        log::info!("...exiting main()");
+        log::info!("entering desktop_main() at {}...", system_now());
+        log::info!("...exiting desktop_main() at {}", system_now());
         Ok(())
     }
 }
 
 #[cfg(not(target_family = "wasm"))]
 pub fn main() -> Result<(), Box<dyn std::error::Error>> {
-    not_wasm::main()
+    not_wasm::desktop_main()
 }
