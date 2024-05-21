70129717b2686624dfdd8029b48003d8b8a3aaba
Koen Willemsen <Koen.Willemsen@materialise.be>
2024-05-21 15:58:13 +0200

    Intermediate commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::sync::Arc;
+use wgpu::{core::instance, *};
 use winit::{application::*, event::*, event_loop::*, window::*};
 
 //
@@ -54,15 +55,34 @@ fn system_now() -> String {
 // Relevant code starts here!
 //
 
+struct GpuState {
+    instance: Instance,
+    device: Device,
+    queue: Queue,
+}
+impl GpuState {
+    #[cfg(not(target_family = "wasm"))]
+    fn from_window(_window: Arc<Window>) -> (Self, Surface<'static>) {
+        todo!()
+    }
+
+    #[cfg(target_family = "wasm")]
+    async fn from_wasm() -> Self {
+        todo!()
+    }
+
+    fn create_surface(&self, window: Arc<Window>) -> Surface<'static> {
+        todo!()
+    }
+}
+
 #[derive(Default)]
 struct App {
     window: Option<Arc<winit::window::Window>>,
+    surface: Option<Surface<'static>>,
+    gpu_state: Option<GpuState>,
 }
 impl App {
-    fn new() -> Self {
-        Self::default()
-    }
-
     #[cfg(target_family = "wasm")]
     fn create_window(event_loop: &ActiveEventLoop) -> Window {
         use wasm_bindgen::prelude::*;
@@ -79,18 +99,48 @@ impl App {
     fn create_window(event_loop: &ActiveEventLoop) -> Window {
         log_result!(event_loop.create_window(Window::default_attributes()))
     }
+
+    fn instance() -> Instance {
+        Instance::new(InstanceDescriptor {
+        backends: Backends::PRIMARY,
+        flags: InstanceFlags::debugging(),
+        dx12_shader_compiler: Dx12Compiler::default(),
+        gles_minor_version: Gles3MinorVersion::default(),
+    })
+    }
+
+    fn new() -> Self {
+        Self::default()
+    }
+
+    async fn init_wasm_gpu(&mut self) {
+        todo!()
+    }
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        // This method is called eg. when the application starts, when the user
+        // browses 'back' to the webpage, when the OS resumes the application...
         log::info!("ApplicationHandler::resumed() for App");
-        if self.window.is_none() {
-            self.window = Some(Arc::new(Self::create_window(event_loop)));
+        let window = self.window.get_or_insert_with(|| Arc::new(Self::create_window(event_loop))).clone();
+        if let Some(gpu_state) = &self.gpu_state {
+            if self.surface.is_none() {
+                self.surface = Some(gpu_state.create_surface(window));
+            }
+        } else {
+            let (gpu_state, surface) = GpuState::from_window(window);
+            self.gpu_state = Some(gpu_state);
+            self.surface = Some(surface);
         }
+
     }
     fn suspended(&mut self, _event_loop: &ActiveEventLoop) {
+        // This method is called eg. when the user browses away from the
+        // webpage, when the OS suspends the application...
         log::info!("ApplicationHandler::suspended() for App");
         self.window = None;
+        self.surface = None;
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
@@ -130,7 +180,9 @@ pub mod wasm {
         let event_loop = log_result!(EventLoop::new());
         event_loop.set_control_flow(ControlFlow::Wait);
         use winit::platform::web::EventLoopExtWebSys;
-        event_loop.spawn_app(App::new());
+        let mut app = App::new();
+        app.init_wasm_gpu();
+        event_loop.spawn_app(app);
         log::info!("...exiting wasm_main() at {}", system_now());
         Ok(())
     }
